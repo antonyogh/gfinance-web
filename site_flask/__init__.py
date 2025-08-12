2d9e6a5c28b24bdb5826f60e96aa1a9a8c65ebe5
antonyogh <antoniocunha2805@gmail.com>
2025-08-12 00:22:21 -0300

    alt

--- site_flask/__init__.py
+++ site_flask/__init__.py
@@ -5,12 +5,12 @@ from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://gfinance_user:U9XIEqi85s4U2x4dpjmeo0qYUqZqNxeR@dpg-d2d9tjjuibrs739dt6c0-a:5432/gfinance'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 #os.getenv('DATABASE_URL','sqlite:///banco.db').replace('postegres://','postegresql://')
 
 app.config['SECRET_KEY']='b2c8707d67548b2df213a261d90cf531f4f1d02e'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app=app)
+db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
